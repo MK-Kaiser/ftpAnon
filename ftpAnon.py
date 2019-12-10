a1c55c026f167457c18ad91b584c587d653c702e
MK-Kaiser <51086253+MK-Kaiser@users.noreply.github.com>
2019-12-10 11:54:27 -0600

    create ftpAnon.py
    tool to test for ftp anonymous login.

---
+++ ftpAnon.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import ftplib
+
+def anonLogin(hostname):
+    try:
+        ftp = ftplib.FTP(hostname)
+        ftp.login('anonymous', 'a@b.com')
+        print('\n[*] ' + str(hostname) ' FTP Anonymous Logon Successful')
+        ftp.quit()
+        return True
+        
+    except Exception as e:
+        print('\n[-] ' + str(hostname) + ' FTP Anonymous Logon Failed')
+        return False
+        
+    host = args.tgtHost
+    anonLogin(host)
+
+def main():
+    parser = argparse.ArgumentParser(description='uses pxssh from pexpect to ssh brute forcing')
+    parser.add_argument('-t', '--tgt', dest='tgtHost', type=str, required=False, help='provide a target url, or address example: www.website.com')
+    parser.add_argument('-v', '--version', dest='ver', required=False, action='store_true', help='display version number.')
+    args = parser.parse_args()
+    if args.ver:
+        print("anonLogin ftp version 0.1")
+        exit()    
+    
+if __name__ == '__main__':
+    main()
